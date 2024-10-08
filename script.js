ac0fc14ef1aa11e077f31245a5d0c7eb139f59be
whiteSoulX <prisom.halder.cse@ulab.edu.bd>
2024-10-09 04:18:52 +0600

    Finaly Mobile Navigation
    Complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function toggleMenu{
-    const menu = document.querySelector(.menu-links);
-    const menu = document.querySelector(.humburger-icon);
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".humburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
