0140050c269562c652f2f7ee0c1a74391773f4aa
whiteSoulX <prisom.halder.cse@ulab.edu.bd>
2024-10-09 03:59:23 +0600

    Mobile Screen Modified
    Not completed

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu{
+    const menu = document.querySelector(.menu-links);
+    const menu = document.querySelector(.humburger-icon);
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
